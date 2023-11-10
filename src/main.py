1192fd4f6334a7a958b5850fd483450802777fa0
Jeongseop Lim <jseoplim@gmail.com>
2023-11-11 03:00:29 +0900

    feat: Include proxy api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from device.apis import router as device_router
+from proxy.apis import router as proxy_router
 
 app = FastAPI()
 app.include_router(device_router)
+app.include_router(proxy_router)

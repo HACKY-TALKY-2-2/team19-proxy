0b4fe3574063eb57576fbf291a0f06199f8f271a
Jeongseop Lim <jseoplim@gmail.com>
2023-11-11 06:49:13 +0900

    feat: Add update device position repository

--- src/device/repositories.py
+++ src/device/repositories.py
@@ -11,4 +11,25 @@ class DeviceSQLRepository:
             INSERT INTO devices (device_token)
             VALUES (:device_token)
             """)
-        self.session.execute(raw_sql, {"device_token": device_token})
+        self.session.execute(
+            raw_sql,
+            {"device_token": device_token},
+        )
+
+    def update_device_position(
+        self, device_token: str, longitude: float, latitude: float
+    ):
+        raw_sql: TextClause = text("""
+            UPDATE devices
+            SET position = ST_GeomFromText('POINT(:latitude :longitude)'),
+                updated_at = NOW()
+            WHERE device_token = :device_token
+            """)
+        self.session.execute(
+            raw_sql,
+            {
+                "device_token": device_token,
+                "longitude": longitude,
+                "latitude": latitude,
+            },
+        )
